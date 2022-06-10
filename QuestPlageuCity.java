1dd874400846f57501fbf10b18c85dc5aff76fa9
thisguyhasaname <47010166+thisguyhasaname@users.noreply.github.com>
2022-06-10 15:14:54 -0500

    Update QuestPlageuCity.java

--- QuestPlageuCity.java
+++ QuestPlageuCity.java
@@ -14,6 +14,10 @@ public class QuestPlagueCity extends Script
     Position dugHole = new Position (2566, 3332, 0);
     Position Grateunder = new Position(2514,9739,0);
     Position EdmondUnder = new Position(2517,9755,0);
+    Position northHouse = new Position(2531, 3328, 0);
+    Position southHouse = new Position(2533,3272,0);
+    Position mayorHouse = new Position(2526,3311,0);
+    Position BravekDoor = new Position(2529,3314,0);
     public int onLoop() throws InterruptedException
     {
 
@@ -58,43 +62,63 @@ public class QuestPlagueCity extends Script
                 dialogues.completeDialogue();
             case 10:
                 inventory.getItem("Gas mask").interact();
-            case 11:
-
-            case 12:
-
-            case 13:
-
-            case 14:
-
-            case 15:
-
-            case 16:
-
-            case 17:
-
-            case 18:
-
-            case 19:
-
+                objects.closest("Pipe").interact();
+                npcs.closest("Jethick").interact();
+                dialogues.completeDialogue("Yes, I'll return it for you.");
             case 20:
-
+                walking.webWalk(northHouse);
+                objects.closest("Door");
+                dialogues.completeDialogue();
             case 21:
-
+                npcs.closest("Martha Rehnison").interact();
+                dialogues.completeDialogue();
             case 22:
-
+                objects.closest("Stairs").interact();
+                npcs.closest("Milli Rehnison").interact();
+                dialogues.completeDialogue();
             case 23:
+                objects.closest("Stairs").interact();
+                objects.closest("Door").interact();
+                walking.webWalk(southHouse);
+                objects.closest("Door").interact();
+                dialogues.completeDialogue("I fear not a mere plague.");
 
             case 24:
+                walking.webWalk(mayorHouse);
+                npcs.closest("Clerk").interact();
+                dialogues.completeDialogue("I need permission to enter a plague house.", "This is urgent though! Someone's been kidnapped!");
 
             case 25:
+                walking.webWalk(BravekDoor);
+                objects.closest("Door").interact();
+                npcs.closest("Bravek").interact();
+                dialogues.completeDialogue("This is really important though!", "Do you know what's in the cure?");
 
             case 26:
-
+                inventory.getItem("Chocolate dust").interact();
+                inventory.getItem("Bucket of milk").interact();
+                inventory.getItem("Snape grass").interact();
+                inventory.getItem("Chocolately milk").interact();
+                inventory.getItem("Hangover cure").interact();
+                npcs.closest("Bravek").interact();
             case 27:
+                dialogues.completeDialogue("They won't listen to me!");
+                walking.webWalk(southHouse);
+                objects.closest("Door").interact();
+                dialogues.completeDialogue();
+                objects.closest("Barrel").interact();
+                objects.closest("Spooky stairs").interact();
+                objects.closest("Door").interact();
+                npcs.closest("Elena").interact();
+                dialogues.completeDialogue();
 
             case 28:
-
-            case 29:
+                objects.closest("Door").interact();
+                objects.closest("Spooky stairs").interact();
+                //might require more walks than this big one
+                walking.webWalk(edmondArea);
+                npcs.closest("Edmond").interact();
+                //quest complete
 
 
         }
